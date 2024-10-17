161bc0fb6670ae0ccf7d246f1c6cec6774ad08b3
jhincapie197 <145515467+jhincapie197@users.noreply.github.com>
2024-10-16 19:36:15 -0500

    Se restaura la RamaN y se modifica linea 5

--- prueba.py
+++ prueba.py
@@ -2,11 +2,5 @@ print("Hola grupo, vamos haber como cambiar un archivo y como eliminarlo")
 num1 = 3
 num2 = 5
 
-suma = num1 + num2
+suma = (num1 + num2) * 5
 print(suma)
-
-#Nueva Rama
-num3 = 10
-num4 = 20
-multiplicacion = num3 * num4
-print(multiplicacion)
